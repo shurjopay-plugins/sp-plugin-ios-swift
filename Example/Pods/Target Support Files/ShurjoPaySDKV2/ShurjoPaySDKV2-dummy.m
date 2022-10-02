6a705ae6bb28f06b1a588b111fd45556444f6483
Rz Rasel <emailrzrasel@gmail.com>
2022-10-02 15:03:40 +0600

    Update plugins

---
+++ Example/Pods/Target Support Files/ShurjoPaySDKV2/ShurjoPaySDKV2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ShurjoPaySDKV2 : NSObject
+@end
+@implementation PodsDummy_ShurjoPaySDKV2
+@end
---
+++ Example/ShurjoPaySDKV2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ShurjoPaySDKV2
+//
+//  Created by Rz Rasel on 10/02/2022.
+//  Copyright (c) 2022 Rz Rasel. All rights reserved.
+//
+
+import UIKit
+import ShurjoPaySDKV2
+
+class ViewController: UIViewController {
+    var shurjopaySdk: ShurjopaySdk?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
